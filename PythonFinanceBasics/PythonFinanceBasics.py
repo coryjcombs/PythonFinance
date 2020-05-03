f5e452239aaf73ed9e69b6b4cc2b854fe721d772
coryjcombs <cory.j.combs@outlook.com>
2020-05-03 11:22:23 -0400

    Set up basic data pull and storage

---
+++ PythonFinanceBasics/PythonFinanceBasics.py
@@ -0,0 +1,18 @@
+# Packages
+import pandas as pd
+import pandas_datareader as pdr
+import datetime
+
+# Data import
+aapl = pdr.get_data_yahoo('AAPL',
+                          start=datetime.datetime(2006, 10, 1),
+                          end=datetime.datetime(2012, 1, 1))
+
+# Data storage (temp)
+aapl.to_csv('data/aapl_ohlc.csv')
+
+# Data reading
+df_aapl = pd.read_csv('data/aapl_ohlc.csv',
+                      header=0,
+                      index_col='Date',
+                      parse_dates=True)
